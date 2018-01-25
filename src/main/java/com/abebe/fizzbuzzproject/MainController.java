d2dd7650080f03257c8ee2c1bc928183b1c8e578
Abebe Asnake <abexasnex@gmail.com>
2018-01-25 14:33:34 -0500

    start and end from user input

--- src/main/java/com/abebe/fizzbuzzproject/MainController.java
+++ src/main/java/com/abebe/fizzbuzzproject/MainController.java
@@ -3,25 +3,33 @@ package com.abebe.fizzbuzzproject;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Scanner;
+
 @RestController
 public class MainController {
     @RequestMapping(value="/")
     public String ShowIndex()
     {
-String value="";
-
-     for(int i=1;i<=100;i++){
+        String value="";
+        int start;
+        int end ;
+        Scanner keyBoard = new Scanner(System.in);
+        System.out.println("Please inter the start number");
+        start = keyBoard.nextInt();
+        System.out.println("please inter the end number");
+        end= keyBoard.nextInt();
+     for(int i=start;i<=end;i++){
          if(i%3 ==0 && i%5==0){
-             value +="FizzBuzz" +"\n";
+             value +="FizzBuzz" +"\r\n";
          }
          else if(i%3==0){
-             value +="Fizz" +"\n";
+             value +="Fizz" +"\r\n";
          }
          else if(i%5==0){
-             value +="Buzz" +"\n";
+             value +="Buzz" +"\r\n";
          }
          else{
-             value += Integer.toString(i)+"\n";
+             value += Integer.toString(i)+"\r\n";
      }
      }
         System.out.println(value);
