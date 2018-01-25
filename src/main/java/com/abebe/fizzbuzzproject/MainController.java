4aa183f3ebe399a8e0d258d75e2eef2029106027
Abebe Asnake <abexasnex@gmail.com>
2018-01-25 15:05:57 -0500

    prompt only the end number

--- src/main/java/com/abebe/fizzbuzzproject/MainController.java
+++ src/main/java/com/abebe/fizzbuzzproject/MainController.java
@@ -2,35 +2,30 @@ package com.abebe.fizzbuzzproject;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.Scanner;
-
 @RestController
 public class MainController {
     @RequestMapping(value="/")
-    public String ShowIndex()
+    public String fizz()
     {
         String value="";
         String number="";
-        int start;
         int end ;
         Scanner keyBoard = new Scanner(System.in);
-        System.out.println("Please inter the start number");
-        start = keyBoard.nextInt();
         System.out.println("please inter the end number");
         end= keyBoard.nextInt();
-     for(int i=start;i<=end;i++){
+     for(int i=1;i<=end;i++){
          if(i%3 ==0 && i%5==0){
-             value +="FizzBuzz" +"\r\n";
+             value +="FizzBuzz" +"\r\n<br/>";
          }
          else if(i%3==0){
-             value +="Fizz" +"\r\n";
+             value +="Fizz" +"\r\n<br/>";
          }
          else if(i%5==0){
-             value +="Buzz" +"\r\n";
+             value +="Buzz" +"\r\n<br/>";
          }
          else{
-             value += Integer.toString(i)+"\r\n";
+             value += Integer.toString(i)+"\r\n<br/>";
      }
          if(i%7==0){
              number +=i+"\n";
---
+++ src/main/java/com/abebe/fizzbuzzproject/calc.java
@@ -0,0 +1,4 @@
+package com.abebe.fizzbuzzproject;
+
+public class calc {
+}
